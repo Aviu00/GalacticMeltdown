60372032d82746921c5b969af179508566161fed
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-01-31 15:31:29 +0700

    Remove tile coordinates (unused)

--- GalacticMeltdown/Tile.cs
+++ GalacticMeltdown/Tile.cs
@@ -3,21 +3,17 @@ using GalacticMeltdown.data;
 
 namespace GalacticMeltdown
 {
-    public class Tile : IDrawable, IImmovable
+    public class Tile : IDrawable
     {
         public char Symbol { get; }
         public ConsoleColor FgColor { get; }
         public ConsoleColor BgColor { get; }
-        public int X { get; }
-        public int Y { get; }
         public bool WasSeenByPlayer = false;
         public bool IsTransparent { get; }
         public bool IsWalkable { get; }
 
         public Tile(TileTypeData tileTypeData, int x, int y)
         {
-            X = x;
-            Y = y;
             Symbol = tileTypeData.Symbol;
             FgColor = tileTypeData.Color;
             BgColor = ConsoleColor.Black;
