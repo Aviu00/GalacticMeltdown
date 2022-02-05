55fa2973c41a5f78bec29322bdd99dfa8d6447d0
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-05 13:16:20 +0700

    Add base View class

---
+++ GalacticMeltdown/View.cs
@@ -0,0 +1,20 @@
+namespace GalacticMeltdown;
+
+public abstract class View
+{
+    public abstract SymbolData GetSymbol(int x, int y);
+
+    private int _width;
+    private int _height;
+
+    protected void SetSize(int width, int height)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    protected View(int initialWidth, int initialHeight)
+    {
+        SetSize(initialWidth, initialHeight);
+    }
+}
