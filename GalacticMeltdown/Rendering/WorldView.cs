de9601e326d8fd0bbed20c5409caeb77210bdafb
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-05 15:37:02 +0700

    Add WorldView

---
+++ GalacticMeltdown/Rendering/WorldView.cs
@@ -0,0 +1,14 @@
+namespace GalacticMeltdown.Rendering;
+
+public class WorldView : View
+{
+    private Map _map;
+    private IHasCoords _focusObject;
+    
+    public WorldView(int initialWidth, int initialHeight, Map map)
+        : base(initialWidth, initialHeight)
+    {
+        _map = map;
+        _focusObject = map.Player;
+    }
+}
