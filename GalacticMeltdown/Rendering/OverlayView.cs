42202b439f36b9ab0f150b3c7c6bf131e8428785
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-06 03:04:20 +0700

    Adjust OverlayView to lack of initial size in constructor

--- GalacticMeltdown/Rendering/OverlayView.cs
+++ GalacticMeltdown/Rendering/OverlayView.cs
@@ -6,19 +6,21 @@ public class OverlayView : View
 {
     private Map _map;  // Minimap, Enemy-in-sight indicator, (?) Event log
     private Player _player;  // State, Effects, (?) Event log, Enemy-in-sight indicator
-    private SymbolData?[,] _symbols; 
+    private SymbolData?[,] _symbols;
+    private int Width = 0;
+    private int Height = 0;
 
     public OverlayView(Map map)
     {
         _map = map;
         _player = map.Player;
-        _symbols = new SymbolData?[Width, Height];
         // TODO: subscribe to events
     }
 
     public override SymbolData GetSymbol(int x, int y)
     {
-        if (_symbols[x, y] is null) return new SymbolData(' ', ConsoleColor.Black, ConsoleColor.Black);
+        if (!(0 <= x && x < Width && 0 <= y && y < Height) || _symbols[x, y] 
+                is null) return new SymbolData(' ', ConsoleColor.Black, ConsoleColor.Black);
         return (SymbolData) _symbols[x, y];
     }
 
