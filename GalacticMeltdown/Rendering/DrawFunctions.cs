23e3f40fa24a7b06a87daf34134a3813a7a63b18
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-06 03:57:57 +0700

    Remove unneeded DrawFunctions

--- GalacticMeltdown/Rendering/DrawFunctions.cs
+++ GalacticMeltdown/Rendering/DrawFunctions.cs
@@ -5,29 +5,6 @@ namespace GalacticMeltdown.Rendering;
 public static class DrawFunctions
 {
     public delegate SymbolData GetSymbolAt(int x, int y);
-    public static SymbolData GetScreenSymbol(int x, int y)
-    {
-        (int x, int y) glCoords = GameManager.Renderer.ConvertScreenToGlobalCoords(x, y);
-        return GetMapSymbol(glCoords.x, glCoords.y);
-    }
-
-    public static SymbolData GetMapSymbol(int x, int y)
-    {
-        IDrawable drawableObj;
-        if (GameManager.Map.Player.VisibleObjects.ContainsKey((x,y)))//if currently visible by player
-        {
-            drawableObj = GameManager.Map.Player.VisibleObjects[(x,y)];
-            return new SymbolData(drawableObj.Symbol, drawableObj.FgColor, drawableObj.BgColor);
-        }
-
-        drawableObj = GameManager.Map.GetTile(x, y);
-        if (drawableObj != null && ((Tile) drawableObj).WasSeenByPlayer)//if not visible by player
-        {
-            return new SymbolData(drawableObj.Symbol, Utility.OutOfVisionTileColor, ConsoleColor.Black);
-        }
-
-        return new SymbolData(' ', Console.ForegroundColor, ConsoleColor.Black);
-    }
 }
 
 public struct SymbolData
