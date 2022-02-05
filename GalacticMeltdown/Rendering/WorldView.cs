ec677e4f338e2b728831f2aac71e14cfc0727b1d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-05 22:53:21 +0700

    Implement WorldView

--- GalacticMeltdown/Rendering/WorldView.cs
+++ GalacticMeltdown/Rendering/WorldView.cs
@@ -1,19 +1,53 @@
+using System;
+using System.Collections.Generic;
+
 namespace GalacticMeltdown.Rendering;
 
 public class WorldView : View
 {
     private Map _map;
     private IHasCoords _focusObject;
+    private int _viewRadius;
+    private Dictionary<(int, int), IDrawable> _visibleObjects;
     
     public WorldView(int initialWidth, int initialHeight, Map map)
         : base(initialWidth, initialHeight)
     {
         _map = map;
         _focusObject = map.Player;
+        _viewRadius = map.Player.ViewRange;
+        _visibleObjects = _map.GetObjectsVisibleAround(_focusObject.X, _focusObject.Y, _viewRadius);
     }
 
     public override SymbolData GetSymbol(int x, int y)
     {
-        
+        (x, y) = Utility.ConvertAbsoluteToRelativeCoords(x, y, Width / 2, Height / 2);
+        (x, y) = Utility.ConvertRelativeToAbsoluteCoords(x, y, _focusObject.X, _focusObject.Y);
+        IDrawable drawableObj;
+        if (_visibleObjects.ContainsKey((x, y)))
+        {
+            drawableObj = _visibleObjects[(x, y)];
+            return new SymbolData(drawableObj.Symbol, drawableObj.FgColor, drawableObj.BgColor);
+        }
+
+        drawableObj = _map.GetTile(x, y);
+        if (drawableObj is not null && ((Tile) drawableObj).WasSeenByPlayer)
+        {
+            return new SymbolData(drawableObj.Symbol, Utility.OutOfVisionTileColor, ConsoleColor.Black);
+        }
+
+        return new SymbolData(' ', ConsoleColor.Black, ConsoleColor.Black);
+    }
+
+    private void MapChange(object sender, EventArgs e)
+    {
+        _visibleObjects = _map.GetObjectsVisibleAround(_focusObject.X, _focusObject.Y, _viewRadius);
+    }
+
+    public void SetFocus(IHasCoords focusObject, int viewRadius)
+    {
+        _focusObject = focusObject;
+        _viewRadius = viewRadius;
+        _visibleObjects = _map.GetObjectsVisibleAround(_focusObject.X, _focusObject.Y, _viewRadius);
     }
 }
