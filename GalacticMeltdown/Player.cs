7ab9aaa5d3c016037f7e5c56676786f8a12d0d1d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-06 18:37:34 +0700

    Add ICanSeeTiles implementation to Player

--- GalacticMeltdown/Player.cs
+++ GalacticMeltdown/Player.cs
@@ -2,27 +2,27 @@ using System;
 
 namespace GalacticMeltdown;
 
-public class Player : IEntity, IControllable
+public class Player : IEntity, IControllable, ICanSeeTiles
 {
     public int X { get; set; }
     public int Y { get; set; }
     public char Symbol { get; }
     public ConsoleColor FgColor { get; }
     public ConsoleColor BgColor { get; }
-    private int _viewRange = 15;
+    private int _viewRadius = 15;
     private Func<int, int, Tile> _tileAt;
 
     public bool NoClip;//Temporary implementation of debugging "cheat codes"
     public bool Xray { get; set; }
 
-    public int ViewRange
+    public int ViewRadius
     {
-        get => _viewRange;
+        get => _viewRadius;
         set
         {
             if (value > 0)
             {
-                _viewRange = value;
+                _viewRadius = value;
             }
         }
     }
