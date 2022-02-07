c62aa66b4562a1c8322a6f0bb49ad03cc6f72f57
Ruslan Abliazov <r,abliazov@gmail.com>
2022-02-07 15:53:22 +0300

    Added AttackStrategu for MeleeEnemy

--- GalacticMeltdown/MeleeEnemy.cs
+++ GalacticMeltdown/MeleeEnemy.cs
@@ -1,14 +1,17 @@
+using System.Dynamic;
 using GalacticMeltdown.EntityBehaviors;
 
 namespace GalacticMeltdown;
 
-public class MeleeEnemy : Enemy, IMoveStrategy
+public class MeleeEnemy : Enemy, IMoveStrategy, IAttackStrategy
 {
     public MoveStrategy MoveStrategy { get; set; }
+    public AttackStrategy AttackStrategy { get; set; }
 
     public MeleeEnemy(int x, int y, Map map, Player player) : base(x, y, map, player)
     {
         MoveStrategy = new MoveStrategy(this, Map);
+        AttackStrategy = new AttackStrategy(this, Map);
     }
 
     protected override void TakeAction(int movePoints)
