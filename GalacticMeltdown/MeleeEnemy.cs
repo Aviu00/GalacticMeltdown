98f1d5d153a913f403762e6633ccbaa19a2faa6b
Ruslan Abliazov <r,abliazov@gmail.com>
2022-02-20 16:11:35 +0300

    Enemy move to point where it saw player last time

--- GalacticMeltdown/MeleeEnemy.cs
+++ GalacticMeltdown/MeleeEnemy.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using GalacticMeltdown.EntityBehaviors;
 
 namespace GalacticMeltdown;
@@ -10,48 +12,71 @@ public class MeleeEnemy : Enemy, IMoveStrategy
 {
     public MoveStrategy MoveStrategy { get; set; }
 
+    protected int LastSeenPlayerX;
+    protected int LastSeenPlayerY;
+
     public MeleeEnemy(int x, int y, Map map, Player player) : base(x, y, map, player)
     {
         MoveStrategy = new MoveStrategy(this, Map);
+        LastSeenPlayerX = X;
+        LastSeenPlayerY = Y;
     }
 
-    protected override void TakeAction(int movePoints)
+    protected bool SeePlayer()
     {
-        this.Energy = movePoints;
         bool flag = true;
-        IEnumerable PathToPlayer = Algorithms.BresenhamGetPointsOnLine(X, Y, this.Player.X, this.Player.Y); 
-        // check if enemy see player
-        foreach (var coords in Algorithms.BresenhamGetPointsOnLine(X, Y, this.Player.X, this.Player.Y))
+        foreach (var coords in Algorithms.BresenhamGetPointsOnLine(this.X, this.Y, Player.X, Player.Y))
         {
             if (!Map.GetTile(coords.x, coords.y).IsWalkable)
             {
                 flag = false;
             }
         }
+        return flag;
+    }
 
+    // makes many moves
+    protected void MoveToGoal(IEnumerable PathToPlayer)
+    {
+        int CostOfWay = 0;
         int DiffX = 0, DiffY = 0;
-        // acts only if see enemy
-        if (flag == true)
+        foreach ((int x, int y) coords in PathToPlayer)
         {
-            int CostOfWay = 0;
-            foreach (var coords in Algorithms.BresenhamGetPointsOnLine(X, Y, this.Player.X, this.Player.Y))
+            CostOfWay += (Map.GetTile(coords.x, coords.y)).TileMoveCost;
+            if (CostOfWay < this.Energy)
             {
-                CostOfWay += (Map.GetTile(coords.x, coords.y)).TileMoveCost;
-                if (CostOfWay < this.Energy)
-                {
-                    continue;
-                }
-                else
-                {
-                    DiffX = coords.x - this.X;
-                    DiffY = coords.y - this.Y;
-                    break;
-                }
+                continue;
             }
-            MoveStrategy.Move(DiffX, DiffY);
+            else
+            {
+                DiffX = coords.x - this.X;
+                DiffY = coords.y - this.Y;
+                break;
+            }
+        }
+        MoveStrategy.Move(DiffX, DiffY);
+    }
+    
+    protected override void TakeAction(int movePoints)
+    {
+        this.Energy = movePoints;
+        IEnumerable PathToPlayer;
+        // acts only if see enemy
+        if (SeePlayer())
+        {
+            PathToPlayer = Algorithms.BresenhamGetPointsOnLine(X, Y, this.Player.X, this.Player.Y);
+            int CostOfWay = 0;
+            LastSeenPlayerX = Player.X;
+            LastSeenPlayerY = Player.Y;
+            MoveToGoal(PathToPlayer);
             // string for test
             //Console.WriteLine("Enenmy №-" + GetHashCode().ToString()+ "|||" + DiffX.ToString() + ":" + DiffY.ToString());
         }
-
+        else
+        {
+            PathToPlayer = Algorithms.BresenhamGetPointsOnLine(X, Y, LastSeenPlayerX, LastSeenPlayerY);
+            MoveToGoal(PathToPlayer);
+        }
+        Console.WriteLine("Enenmy №-" + GetHashCode().ToString()+ "|||" + (LastSeenPlayerX - this.X).ToString() + ":" + (LastSeenPlayerY - this.Y).ToString());
     }
 }
