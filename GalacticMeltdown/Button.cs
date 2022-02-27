6a22bebaea07c54e3209763bc6caaf97ac9d25bd
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-27 21:46:08 +0300

    Add Button

---
+++ GalacticMeltdown/Button.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace GalacticMeltdown;
+
+public readonly struct Button
+{
+    public string TextLeft { get; }
+    public string TextRight { get; }
+    private readonly Action _action;
+
+    public Button(string textLeft, string textRight, Action action)
+    {
+        TextLeft = textLeft;
+        TextRight = textRight;
+        _action = action;
+    }
+
+    public void Press() => _action();
+}
