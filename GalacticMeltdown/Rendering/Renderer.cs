07fcba23c79bfa650b9b76c9942e9e987d1f7180
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-02-28 23:47:41 +0300

    Add pixel functions

--- GalacticMeltdown/Rendering/Renderer.cs
+++ GalacticMeltdown/Rendering/Renderer.cs
@@ -10,6 +10,8 @@ public delegate void ViewChangedEventHandler(View sender);
 public class Renderer
 {
     private LinkedList<(View, double, double, double, double)> _views;  // View, top-left and bottom-right corner coords (rel)
+    private LinkedList<Func<ScreenCellData>>[,] _pixelFuncs;
+    private ScreenCellData[,] _screenCells;
 
     public Renderer()
     {
@@ -17,6 +19,29 @@ public class Renderer
         Console.BackgroundColor = ConsoleColor.Black;
         Console.Clear();
         _views = new LinkedList<(View, double, double, double, double)>();
+    }
+
+    private void RecalculatePixelArrays()
+    {
+        int windowWidth = Console.WindowWidth;
+        int windowHeight = Console.WindowHeight;
+        _screenCells = new ScreenCellData[windowWidth, windowHeight];
+        _pixelFuncs = new LinkedList<Func<ScreenCellData>>[windowWidth, windowHeight];
+        foreach (var (view, x0Portion, y0Portion, x1Portion, y1Portion) in _views)
+        {
+            int x0Screen = (int) Math.Round(windowWidth * x0Portion);
+            int y0Screen = (int) Math.Round(windowHeight * y0Portion);
+            int x1Screen = (int) Math.Round(windowWidth * x1Portion);
+            int y1Screen = (int) Math.Round(windowHeight * y1Portion);
+            view.Resize(x1Screen - x0Screen, y1Screen - y0Screen);
+            for (int x = x0Screen; x < x1Screen; x++)
+            {
+                for (int y = y0Screen; y < y1Screen; y++)
+                {
+                    //_pixelFuncs[x, y].AddFirst(() => view.GetSymbol(x - x0Screen, y - y0Screen));
+                }
+            }
+        }
     }
 
     public void AddView(View view, double x0Portion, double y0Portion, double x1Portion, double y1Portion)
