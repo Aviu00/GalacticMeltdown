f1e1f314b02269fa01bab37530b94a4f7ed7be1f
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-01 22:45:08 +0300

    Add FilesystemLevelManager

---
+++ GalacticMeltdown/FilesystemLevelManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace GalacticMeltdown;
+
+public readonly record struct LevelInfo(string Path, int Seed, string Name); 
+
+public class FilesystemLevelManager
+{
+    public List<LevelInfo> GetLevels()
+    {
+        
+    }
+
+    public void RemoveLevel(string path)
+    {
+        
+    }
+
+    public void SaveLevel(Map level)
+    {
+        
+    }
+}
