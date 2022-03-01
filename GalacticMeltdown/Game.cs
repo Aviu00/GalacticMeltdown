01d6dcc1e498e4a077a0218752d38cd87accf880
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-01 19:09:14 +0300

    Implement MainMenu control

--- GalacticMeltdown/Game.cs
+++ GalacticMeltdown/Game.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using GalacticMeltdown.Data;
 using GalacticMeltdown.Rendering;
 
 namespace GalacticMeltdown;
@@ -11,13 +14,33 @@ public static class Game
         while (_playing)
         {
             OpenMainMenu();
+            InputProcessor.StartProcessLoop();
         }
         Renderer.CleanUp();
     }
 
     private static void OpenMainMenu()
     {
-        var session = new PlaySession();
-        session.Start();
+        var playButton = new Button("Play", "", null);
+        var quitButton = new Button("Quit", "", Quit);
+        LinkedList<Button> mainMenuButtons = new();
+        mainMenuButtons.AddLast(playButton);
+        mainMenuButtons.AddLast(quitButton);
+        var mainMenuButtonsView = new ButtonListView(mainMenuButtons);
+        Renderer.AddView(mainMenuButtonsView, 0, 0, 1, 1);
+        InputProcessor.AddBinding(DataHolder.CurrentBindings.Selection, new Dictionary<SelectionControl, Action>
+        {
+            {SelectionControl.Up, mainMenuButtonsView.SelectPrev},
+            {SelectionControl.Down, mainMenuButtonsView.SelectNext},
+            {SelectionControl.Select, mainMenuButtonsView.PressCurrent}
+        });
+        //var session = new PlaySession();
+        //session.Start();
+    }
+
+    private static void Quit()
+    {
+        _playing = false;
+        InputProcessor.StopProcessLoop();
     }
 }
