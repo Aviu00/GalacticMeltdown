0b45e2486aef0010ba4adcbeb7b3306c0c1e6a8b
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-01 10:26:52 +0300

    Make it possible for View to be a dictionary key

--- GalacticMeltdown/Rendering/View.cs
+++ GalacticMeltdown/Rendering/View.cs
@@ -8,12 +8,25 @@ public abstract class View
     public abstract event ViewChangedEventHandler ViewChanged;
     public abstract event CellsChangedEventHandler CellsChanged;
 
+    protected View()
+    {
+        _id = Utility.RandomString(16);
+    }
+
     protected int Width;
     protected int Height;
+    private readonly string _id;
 
     public virtual void Resize(int width, int height)  // Note: should not fire ViewChanged events
     {
         Width = width;
         Height = height;
+    }
+
+    public override int GetHashCode() => _id.GetHashCode();
+
+    public override bool Equals(object obj)
+    {
+        return ReferenceEquals(this, obj);
     }
 }
