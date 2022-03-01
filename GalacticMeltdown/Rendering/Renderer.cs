1bab52de044ab327592c4cf2fb0f2fb258f0e33c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-01 09:31:20 +0300

    Remove now-unused methods DrawArea and PutSymbolAt

--- GalacticMeltdown/Rendering/Renderer.cs
+++ GalacticMeltdown/Rendering/Renderer.cs
@@ -145,63 +145,6 @@ public class Renderer
         UpdateCurrentCells();
         OutputCells();
     }
-
-    public void DrawArea
-        (int startX, int startY, int maxX, int maxY, GetSymbolAt getSymbolAt)
-    {
-        Console.SetCursorPosition(startX, startY);
-        var symbolData = getSymbolAt(startX, startY);
-        StringBuilder currentSequence = new StringBuilder($"{symbolData.Symbol}");
-        ConsoleColor curFgColor = symbolData.FgColor, curBgColor = symbolData.BgColor;
-        int x = startX + 1;
-        for (int areaY = maxY; areaY >= startY; areaY--)
-        {
-            if (startX != 0)  // Can't append new line at end of drawn line
-                Console.SetCursorPosition(startX, ConvertToConsoleY(areaY));
-            for (; x <= maxX; x++)
-            {
-                symbolData = getSymbolAt(x, areaY);
-                if ((symbolData.FgColor != curFgColor && symbolData.Symbol != ' ' || symbolData.BgColor != curBgColor) 
-                    && currentSequence.Length != 0)  // When new line isn't appended length can be 0
-                {
-                    SetConsoleColor(curFgColor, curBgColor);
-                    Console.Write(currentSequence);
-                    currentSequence.Clear();
-                    curFgColor = symbolData.FgColor;
-                    curBgColor = symbolData.BgColor;
-                }
-
-                currentSequence.Append(symbolData.Symbol);
-            }
-
-            x = startX;
-
-            if (startX == 0)  // Can append new line at end of drawn line
-            {
-                currentSequence.Append('\n');
-            }
-            else
-            {
-                SetConsoleColor(curFgColor, curBgColor);
-                Console.Write(currentSequence);
-                currentSequence.Clear();
-            }
-        }
-
-        if (currentSequence.Length != 0)
-        {
-            if (startX == 0) currentSequence.Length--; // remove new line
-            SetConsoleColor(curFgColor, curBgColor);
-            Console.Write(currentSequence);
-        }
-    }
-
-    public void PutSymbolAt(int x, int y, ScreenCellData screenCellData)
-    {
-        Console.SetCursorPosition(x, ConvertToConsoleY(y));
-        SetConsoleColor(screenCellData.FgColor, screenCellData.BgColor);
-        Console.Write(screenCellData.Symbol);
-    }
     
     public static void SetConsoleColor(ConsoleColor fgColor, ConsoleColor bgColor)
     {
