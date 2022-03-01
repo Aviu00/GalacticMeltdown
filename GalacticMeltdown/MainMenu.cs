9643411508ad7bc087de2bedcd1b2a49ef11db34
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-01 18:22:45 +0300

    Add main menu controls

--- GalacticMeltdown/MainMenu.cs
+++ GalacticMeltdown/MainMenu.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Collections.Generic;
+using GalacticMeltdown.Rendering;
+
 namespace GalacticMeltdown;
 
 public class MainMenu
 {
+    private ButtonListView _buttons;
+    
     public void Start()
     {
-        
+        InputProcessor.AddBinding(Data.Data.CurrentBindings.Selection, new Dictionary<SelectionControl, Action>
+        {
+            {SelectionControl.Up, _buttons.SelectPrev},
+            {SelectionControl.Down, _buttons.SelectNext},
+            {SelectionControl.Select, _buttons.PressCurrent}
+        });
     }
 }
