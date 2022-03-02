c8dfbfd6b3201a595b55f7d38621e70f49ef7226
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-02 14:23:03 +0300

    Split PlaySession constructor and start

--- GalacticMeltdown/PlaySession.cs
+++ GalacticMeltdown/PlaySession.cs
@@ -11,19 +11,22 @@ public partial class PlaySession
     private static Map _map;
     private static WorldView _worldView;
 
-    public void Start()
+    public PlaySession(Map level)
     {
-        var map = GenerateMap();
+        _map = level;
         _player = _map.Player;
         _controlledObject = _player;
-        _worldView = new WorldView(map);
+        _worldView = new WorldView(_map);
         _worldView.AddTileRevealingObject(_player);
         _worldView.SetFocus(_player);
+    }
+
+    public void Start()
+    {
         Renderer.AddView(_worldView, 0, 0, 0.8, 1);
-        Renderer.AddView(new OverlayView(map), 0.8, 0, 1, 1);
+        Renderer.AddView(new OverlayView(_map), 0.8, 0, 1, 1);
         Renderer.Redraw();
         InputProcessor.AddBinding(DataHolder.CurrentBindings.Player, PlayerActions);
-        InputProcessor.StartProcessLoop();
     }
     
     private static void MoveControlled(int deltaX, int deltaY)
