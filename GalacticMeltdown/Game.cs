a0d6c662c0c63e4d81a54fc24a55940b71a6b2bf
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-04 01:55:10 +0300

    Use separate object to manage menus

--- GalacticMeltdown/Game.cs
+++ GalacticMeltdown/Game.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using GalacticMeltdown.Data;
 using GalacticMeltdown.Rendering;
 
 namespace GalacticMeltdown;
@@ -9,13 +5,12 @@ namespace GalacticMeltdown;
 public static class Game
 {
     private static bool _playing;
-    private static Stack<View> _menus;
+    private static MenuView _menu;
     public static void Main()
     {
         _playing = true;
         while (_playing)
         {
-            _menus = new Stack<View>();
             OpenMainMenu();
             InputProcessor.StartProcessLoop();
         }
@@ -32,48 +27,10 @@ public static class Game
 
     private static void OpenMainMenu()
     {
-        OpenBasicMenu(new Button("Select level", "", OpenLevelMenu), new Button("Quit", "", Quit));
-    }
-
-    private static void OpenLevelMenu()
-    {
-        var levelMenu = new LevelManagementView();
-        OpenMenu(levelMenu, new Dictionary<SelectionControl, Action>
-        {
-            {SelectionControl.Up, levelMenu.SelectPrev},
-            {SelectionControl.Down, levelMenu.SelectNext},
-            {SelectionControl.SwitchButtonGroup, levelMenu.SwitchButtonGroup},
-            {SelectionControl.Select, levelMenu.PressCurrent},
-            {SelectionControl.Back, CloseOpenedMenu}
-        });
-    }
-
-    private static void OpenBasicMenu(params Button[] buttons)
-    {
-        var buttonListView = new ButtonListView(buttons);
-        OpenMenu(buttonListView, new Dictionary<SelectionControl, Action>
-        {
-            {SelectionControl.Up, buttonListView.SelectPrev},
-            {SelectionControl.Down, buttonListView.SelectNext},
-            {SelectionControl.Select, buttonListView.PressCurrent}
-        });
-    }
-
-    private static void OpenMenu(View view, Dictionary<SelectionControl, Action> bindings)
-    {
-        if (_menus.Any()) Renderer.RemoveLastView();
-        _menus.Push(view);
-        InputProcessor.AddBinding(DataHolder.CurrentBindings.Selection, bindings);
-        Renderer.AddView(view, 0, 0, 1, 1);
-        Renderer.Redraw();
-    }
-
-    private static void CloseOpenedMenu()
-    {
-        Renderer.RemoveLastView();
-        InputProcessor.RemoveLastBinding();
-        _menus.Pop();
-        Renderer.AddView(_menus.Peek(), 0, 0, 1, 1);
+        _menu = new MenuView();
+        Renderer.AddView(_menu, 0, 0, 1, 1);
+        _menu.OpenBasicMenu(new Button("Select level", "", _menu.OpenLevelMenu), 
+            new Button("Quit", "", Quit));
     }
 
     private static void Quit()
---
+++ GalacticMeltdown/MenuView.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GalacticMeltdown.Data;
+using GalacticMeltdown.Rendering;
+
+namespace GalacticMeltdown;
+
+public class MenuView : View
+{
+    public override event ViewChangedEventHandler NeedRedraw;
+    public override event CellsChangedEventHandler CellsChanged;
+    private Stack<View> _menus = new();
+
+    public override ViewCellData GetSymbol(int x, int y)
+    {
+        return _menus.Any() ? _menus.Peek().GetSymbol(x, y) : new ViewCellData(null, null);
+    }
+
+    public override void Resize(int width, int height)
+    {
+        base.Resize(width, height);
+        foreach (var menu in _menus)
+        {
+            menu.Resize(width, height);
+        }
+    }
+
+    public void OpenMenu(View menu, Dictionary<SelectionControl, Action> bindings)
+    {
+        _menus.Push(menu);
+        menu.Resize(Width, Height);
+        menu.NeedRedraw += SendRedraw;
+        menu.CellsChanged += SendAnim;
+        NeedRedraw?.Invoke(this);
+        InputProcessor.AddBinding(DataHolder.CurrentBindings.Selection, bindings);
+        Renderer.Redraw();
+    }
+
+    public void CloseMenu()
+    {
+        View menu = _menus.Pop();
+        menu.NeedRedraw -= SendRedraw;
+        menu.CellsChanged -= SendAnim;
+        NeedRedraw?.Invoke(this);
+        InputProcessor.RemoveLastBinding();
+    }
+    
+    public void OpenBasicMenu(params Button[] buttons)
+    {
+        var buttonListView = new ButtonListView(buttons);
+        OpenMenu(buttonListView, new Dictionary<SelectionControl, Action>
+        {
+            {SelectionControl.Up, buttonListView.SelectPrev},
+            {SelectionControl.Down, buttonListView.SelectNext},
+            {SelectionControl.Select, buttonListView.PressCurrent}
+        });
+    }
+
+    public void OpenLevelMenu()
+    {
+        var levelMenu = new LevelManagementView();
+        OpenMenu(levelMenu, new Dictionary<SelectionControl, Action>
+        {
+            {SelectionControl.Up, levelMenu.SelectPrev},
+            {SelectionControl.Down, levelMenu.SelectNext},
+            {SelectionControl.SwitchButtonGroup, levelMenu.SwitchButtonGroup},
+            {SelectionControl.Select, levelMenu.PressCurrent},
+            {SelectionControl.Back, CloseMenu}
+        });
+    }
+
+    private void SendRedraw(View view) => NeedRedraw?.Invoke(this);
+
+    private void SendAnim((View, HashSet<(int, int, ViewCellData)>) animInfo) 
+        => CellsChanged?.Invoke((this, animInfo.Item2));
+}
