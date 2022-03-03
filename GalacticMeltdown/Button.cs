c77d6a7f758639bae85899a9e2d5aec22b792c91
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-03 18:48:59 +0300

    Put text building logic into Button

--- GalacticMeltdown/Button.cs
+++ GalacticMeltdown/Button.cs
@@ -16,4 +16,41 @@ public readonly struct Button
     }
 
     public void Press() => _action?.Invoke();
+    
+    
+    public string MakeText(int width)
+    {
+        const string ellipsis = "...";
+        const string separator = "  ";
+        const string noSpaceForRightText = $"{separator}{ellipsis}";
+        int maxLeftStringLength = width - noSpaceForRightText.Length;
+        string screenText;
+        if (TextRight.Length == 0)
+        {
+            screenText = TextLeft.Length > width
+                ? TextLeft.Substring(0, width - ellipsis.Length) + ellipsis 
+                : TextLeft.PadRight(width);
+        }
+        else if (TextLeft.Length == 0)
+        {
+            screenText = TextRight.Length > width
+                ? ellipsis + TextRight.Substring(TextRight.Length - (width - ellipsis.Length))
+                : TextRight.PadLeft(width);
+        }
+        else if (TextLeft.Length >= maxLeftStringLength)
+        {
+            screenText = TextLeft.Substring(0, maxLeftStringLength) + noSpaceForRightText;
+        }
+        else
+        {
+            screenText = TextLeft;
+            screenText += separator;
+            int spaceLeft = width - screenText.Length;
+            screenText += TextRight.Length > spaceLeft
+                ? TextRight.Substring(0, spaceLeft - ellipsis.Length) + ellipsis
+                : TextRight.PadLeft(spaceLeft);
+        }
+
+        return screenText;
+    }
 }
--- GalacticMeltdown/Rendering/ButtonListView.cs
+++ GalacticMeltdown/Rendering/ButtonListView.cs
@@ -29,60 +29,9 @@ public class ButtonListView : View
 
     private void CalculateVisibleButtonText()
     {
-        if (Width < 8)
-        {
-            string placeHolder = Width < 3 ? "" : "...";
-            for (int i = 0; i < _buttonText.Length; i++)
-                _buttonText[i] = placeHolder;
-            return;
-        }
-
         for (int i = 0; i < _buttonText.Length; i++)
         {
-            const string ellipsis = "...";
-            const string separator = "  ";
-            const string noSpaceForRightText = $"{separator}{ellipsis}";
-            string rightText = _buttons[i].TextRight;
-            int maxLeftStringLength = Width - noSpaceForRightText.Length;
-            string leftText = _buttons[i].TextLeft;
-            string screenText;
-            if (rightText.Length == 0)
-            {
-                screenText = leftText.Length > Width - ellipsis.Length 
-                    ? leftText.Substring(0, Width - ellipsis.Length) 
-                    : leftText;
-            }
-            else if (leftText.Length == 0)
-            {
-                if (rightText.Length < Width)
-                {
-                    screenText = string.Join("", Enumerable.Repeat(" ", Width - rightText.Length)) + rightText;
-                }
-                else
-                {
-                    screenText = ellipsis + rightText.Substring(rightText.Length - (Width - ellipsis.Length));
-                }
-            }
-            else if (leftText.Length >= maxLeftStringLength)
-            {
-                screenText = leftText.Substring(0, maxLeftStringLength) + noSpaceForRightText;
-            }
-            else
-            {
-                screenText = leftText;
-                screenText += separator;
-                int spaceLeft = Width - screenText.Length;
-                if (rightText.Length >= spaceLeft)
-                {
-                    screenText += rightText.Substring(0, spaceLeft - ellipsis.Length) + ellipsis;
-                }
-                else
-                {
-                    screenText += string.Join("", Enumerable.Repeat(" ", spaceLeft - rightText.Length)) + rightText;
-                }
-            }
-
-            _buttonText[i] = screenText;
+            _buttonText[i] = _buttons[i].MakeText(Width);
         }
     }
 
