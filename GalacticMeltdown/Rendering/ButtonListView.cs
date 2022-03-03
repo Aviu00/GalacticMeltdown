1b0fb93409732c463967465152689155e5b1eed2
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-03 16:57:05 +0300

    Determine top button index in ButtonListView

--- GalacticMeltdown/Rendering/ButtonListView.cs
+++ GalacticMeltdown/Rendering/ButtonListView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GalacticMeltdown.Rendering;
@@ -8,12 +9,14 @@ public class ButtonListView : View
     public override event CellsChangedEventHandler CellsChanged;
     private readonly List<Button> _buttons;
     private int _currentButtonIndex;
+    private int _topVisibleButtonIndex;
 
     public ButtonListView(ICollection<Button> buttons)
     {
         // TODO: this should have at least one button, check that
         _buttons = new List<Button>(buttons);
         _currentButtonIndex = 0;
+        _topVisibleButtonIndex = 0;
     }
 
     public override ViewCellData GetSymbol(int x, int y)
@@ -29,12 +32,14 @@ public class ButtonListView : View
     public void SelectNext()
     {
         _currentButtonIndex = (_currentButtonIndex + 1) % _buttons.Count;
+        _topVisibleButtonIndex = Math.Max(0, _currentButtonIndex - Height + 1);
         // TODO: update view
     }
 
     public void SelectPrev()
     {
         _currentButtonIndex = (_buttons.Count + _currentButtonIndex - 1) % _buttons.Count;  // -1 should wrap
+        _topVisibleButtonIndex = Math.Max(0, _currentButtonIndex - Height + 1);
         // TODO: update view
     }
 }
