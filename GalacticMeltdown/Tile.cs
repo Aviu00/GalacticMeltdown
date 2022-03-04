c6eebf15831828c0450552c78e70018fabee8b2d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-04 20:00:38 +0300

    Convert Tile to file-scoped namespace

--- GalacticMeltdown/Tile.cs
+++ GalacticMeltdown/Tile.cs
@@ -1,27 +1,26 @@
 using System;
 using GalacticMeltdown.Data;
 
-namespace GalacticMeltdown
-{
-    public class Tile : IDrawable
-    {
-        public (char symbol, ConsoleColor color) SymbolData { get; }
-        public ConsoleColor? BgColor { get; }
-        public bool Seen = false;
-        public bool IsTransparent { get; }
-        public bool IsWalkable { get; }
-        public bool ConnectToWalls { get; } //used in level generation
-        
-        public string Name { get; }
+namespace GalacticMeltdown;
 
-        public Tile(TileTypeData tileTypeData)
-        {
-            SymbolData = (tileTypeData.Symbol, tileTypeData.Color);
-            BgColor = null;
-            IsTransparent = tileTypeData.IsTransparent;
-            IsWalkable = tileTypeData.IsWalkable;
-            ConnectToWalls = tileTypeData.IsConnection;
-            Name = tileTypeData.Name;
-        }
+public class Tile : IDrawable
+{
+    public (char symbol, ConsoleColor color) SymbolData { get; }
+    public ConsoleColor? BgColor { get; }
+    public bool Seen = false;
+    public bool IsTransparent { get; }
+    public bool IsWalkable { get; }
+    public bool ConnectToWalls { get; } //used in level generation
+        
+    public string Name { get; }
+
+    public Tile(TileTypeData tileTypeData)
+    {
+        SymbolData = (tileTypeData.Symbol, tileTypeData.Color);
+        BgColor = null;
+        IsTransparent = tileTypeData.IsTransparent;
+        IsWalkable = tileTypeData.IsWalkable;
+        ConnectToWalls = tileTypeData.IsConnection;
+        Name = tileTypeData.Name;
     }
 }
