0895be18ace04bc0a2b8f05064407b2de5365631
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-04 17:43:40 +0300

    Add more dummy levels for testing

--- GalacticMeltdown/FilesystemLevelManager.cs
+++ GalacticMeltdown/FilesystemLevelManager.cs
@@ -8,7 +8,30 @@ public static class FilesystemLevelManager
 {
     public static List<LevelInfo> GetLevelButtonInfo()
     {
-        return new List<LevelInfo> {new(".", 0, "ExampleName")};
+        return new List<LevelInfo> {new(".", 0, "ExampleName0"), 
+            new("..", 5, "ExampleName1"),
+            new("..", 5, "ExampleName2"),
+            new("..", 5, "ExampleName3"),
+            new("..", 5, "ExampleName4"),
+            new("..", 5, "ExampleName5"),
+            new("..", 5, "ExampleName6"),
+            new("..", 5, "ExampleName7"),
+            new("..", 5, "ExampleName8"),
+            new("..", 5, "ExampleName9"),
+            new("..", 5, "ExampleName10"),
+            new("..", 5, "ExampleName11"),
+            new("..", 5, "ExampleName12"),
+            new("..", 5, "ExampleName13"),
+            new("..", 5, "ExampleName14"),
+            new("..", 5, "ExampleName15"),
+            new("..", 5, "ExampleName16"),
+            new("..", 5, "ExampleName17"),
+            new("..", 5, "ExampleName18"),
+            new("..", 5, "ExampleName19"),
+            new("..", 5, "ExampleName20"),
+            new("..", 5, "ExampleName21"),
+            new("..", 5, "ExampleName22"),
+        };
     }
 
     public static void RemoveLevel(string path)
