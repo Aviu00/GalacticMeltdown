95643dd2d70190331fbc3d07214baab7cffeda04
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-04 18:09:56 +0300

    Implement level removal

--- GalacticMeltdown/Rendering/LevelManagementView.cs
+++ GalacticMeltdown/Rendering/LevelManagementView.cs
@@ -8,7 +8,8 @@ public class LevelManagementView : View
 {
     public override event ViewChangedEventHandler NeedRedraw;
     public override event CellsChangedEventHandler CellsChanged;
-    private readonly List<Button> _levelButtons;
+    private List<LevelInfo> _levelInfos;
+    private List<Button> _levelButtons;
     private readonly List<Button> _managementButtons;
     private int _levelIndex;
     private int _managementIndex;
@@ -25,9 +26,9 @@ public class LevelManagementView : View
 
     public LevelManagementView()
     {
-        List<LevelInfo> levels = FilesystemLevelManager.GetLevelButtonInfo();
-        _levelButtons = new List<Button>(levels.Count);
-        foreach (var levelInfo in levels)
+        _levelInfos = FilesystemLevelManager.GetLevelButtonInfo();
+        _levelButtons = new List<Button>(_levelInfos.Count);
+        foreach (var levelInfo in _levelInfos)
         {
             _levelButtons.Add(new Button(levelInfo.Name, $"seed: {levelInfo.Seed}",
                 () => TryStartLevel(levelInfo.Path)));
@@ -38,14 +39,36 @@ public class LevelManagementView : View
         _managementButtons = new List<Button>
         {
             new Button("Create", "", null),
-            new Button("Delete", "", null)
+            new Button("Delete", "", DeleteLevel)
         };
         _managementButtonText = new string[_managementButtons.Count];
         
         _levelIndex = 0;
         _managementIndex = 0;
         
-        _isManagementSelected = levels.Count == 0;  // can't select a level when none exist
+        _isManagementSelected = _levelInfos.Count == 0;  // can't select a level when none exist
+    }
+
+    private void DeleteLevel()
+    {
+        if (!_levelButtons.Any())
+        {
+            // TODO: error animation
+            return;
+        }
+        // TODO: confirmation dialog
+        FilesystemLevelManager.RemoveLevel(_levelInfos[_levelIndex].Path);
+        _levelInfos.RemoveAt(_levelIndex);
+        _levelButtons.RemoveAt(_levelIndex);
+        if (!_levelButtons.Any())
+        {
+            _isManagementSelected = true;
+            _levelIndex = 0;
+        }
+        else if (_levelIndex >= _levelButtons.Count)
+        {
+            _levelIndex = _levelButtons.Count - 1;
+        }
     }
 
     private void TryStartLevel(string path)
@@ -94,15 +117,23 @@ public class LevelManagementView : View
     public void SelectNext()
     {
         if (_isManagementSelected) _managementIndex = (_managementIndex + 1) % _managementButtons.Count;
-        else _levelIndex = (_levelIndex + 1) % _levelButtons.Count;
+        else if (_levelButtons.Any()) _levelIndex = (_levelIndex + 1) % _levelButtons.Count;  // Avoid zero division
         UpdateOutVars();
         NeedRedraw?.Invoke(this);
     }
 
     public void SelectPrev()
     {
-        if (_isManagementSelected) _managementIndex = (_managementButtons.Count + _managementIndex - 1) % _managementButtons.Count;
-        else _levelIndex = (_levelButtons.Count + _levelIndex - 1) % _levelButtons.Count;
+        if (_isManagementSelected)
+        {
+            _managementIndex -= 1;
+            if (_managementIndex < 0) _managementIndex = _managementButtons.Count - 1;
+        }
+        else if (_levelButtons.Any())
+        {
+            _levelIndex -= 1;
+            if (_levelIndex < 0) _levelIndex = _levelButtons.Count - 1;
+        }
         UpdateOutVars();
         NeedRedraw?.Invoke(this);
     }
