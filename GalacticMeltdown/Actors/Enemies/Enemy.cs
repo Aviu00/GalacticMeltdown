3d23def16148deb2fca63e72d50608336b540866
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-07 21:33:42 +0300

    Add LootTableId to Enemy

--- GalacticMeltdown/Actors/Enemies/Enemy.cs
+++ GalacticMeltdown/Actors/Enemies/Enemy.cs
@@ -2,11 +2,9 @@ namespace GalacticMeltdown.Actors.Enemies;
 
 public abstract class Enemy : Npc
 {
-    protected readonly Player Player;
-
+    public string LootTableId { get; init; }
     protected Enemy(int maxHp, int maxEnergy, int dex, int def, int x, int y, LevelRelated.Level level) 
         : base(maxHp, maxEnergy, dex, def, x, y, level)
     {
-        Player = level.Player;
     }
 }
