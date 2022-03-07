c9115872e76a1e603ac044adf02a729315553f09
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-08 02:04:53 +0300

    Rearrange ChunkGenerator

--- GalacticMeltdown/MapGeneration/ChunkGenerator.cs
+++ GalacticMeltdown/MapGeneration/ChunkGenerator.cs
@@ -9,18 +9,18 @@ namespace GalacticMeltdown.MapGeneration;
 public class ChunkGenerator
 {
     private const int ChunkSize = DataHolder.ChunkSize;
-    
+
     public bool HasAccessToMainRoute;
     public bool MainRoute;
-    
+
     public ChunkGenerator NorthConnection = null;
     public ChunkGenerator EastConnection = null;
     public ChunkGenerator SouthConnection = null;
     public ChunkGenerator WestConnection = null;
-    
+
     public bool IsStartPoint;
     public bool IsEndPoint;
-    
+
     public double Difficulty = -1;
 
     public int ConnectionCount =>
@@ -37,26 +37,6 @@ public class ChunkGenerator
     {
         MapX = x;
         MapY = y;
-    }
-
-    public ChunkGenerator GetNextRoom(ChunkGenerator previousRoom)
-    {
-        if (NorthConnection is not null && NorthConnection != previousRoom) return NorthConnection;
-        if (EastConnection is not null && EastConnection != previousRoom) return EastConnection;
-        if (SouthConnection is not null && SouthConnection != previousRoom) return SouthConnection;
-        return WestConnection!;
-    }
-
-    public void AccessMainRoute(double mainRouteDifficulty)
-    {
-        if (MainRoute || Difficulty >= mainRouteDifficulty) return;
-
-        HasAccessToMainRoute = true;
-        Difficulty = mainRouteDifficulty;
-        NorthConnection?.AccessMainRoute(Difficulty);
-        EastConnection?.AccessMainRoute(Difficulty);
-        SouthConnection?.AccessMainRoute(Difficulty);
-        WestConnection?.AccessMainRoute(Difficulty);
     }
 
     public Chunk GenerateChunk(TileTypeData[,] roomData, Tile[,] northernTileMap = null, Tile[,] easternTileMap = null)
@@ -86,62 +66,24 @@ public class ChunkGenerator
         return new Chunk(Tiles, Difficulty);
     }
 
-    private void FillBorderWalls(TileTypeData[,] roomData, Tile[,] northernTileMap, Tile[,] easternTileMap)
+    public void AccessMainRoute(double mainRouteDifficulty)
     {
-        for (int x = 0; x < ChunkSize; x++)
-        {
-            TileTypeData terrainObject = NorthConnection is null || x is not (ChunkSize / 2 - 1 or ChunkSize / 2)
-                ? GetConnectableData(roomData, x, ChunkSize - 1, northernTileMap?[x, 0].ConnectToWalls, overrideId: "wall")
-                : DataHolder.TileTypes["floor"]; //will be changed to "door" later
-            Tiles[x, ChunkSize - 1] = new Tile(terrainObject);
-        }
+        if (MainRoute || Difficulty >= mainRouteDifficulty) return;
 
-        for (int y = 0; y < ChunkSize - 1; y++)
-        {
-            TileTypeData terrainObject = EastConnection is null || y is not (ChunkSize / 2 - 1 or ChunkSize / 2)
-                ? GetConnectableData(roomData, ChunkSize - 1, y, easternTileConnectable: easternTileMap?[0, y].ConnectToWalls,
-                    overrideId: "wall")
-                : DataHolder.TileTypes["floor"]; //will be changed to "door" later
-            Tiles[ChunkSize - 1, y] = new Tile(terrainObject);
-        }
+        HasAccessToMainRoute = true;
+        Difficulty = mainRouteDifficulty;
+        NorthConnection?.AccessMainRoute(Difficulty);
+        EastConnection?.AccessMainRoute(Difficulty);
+        SouthConnection?.AccessMainRoute(Difficulty);
+        WestConnection?.AccessMainRoute(Difficulty);
     }
 
-    private TileTypeData GetConnectableData(TileTypeData[,] roomData, int x, int y,
-        bool? northernTileConnectable = null, bool? easternTileConnectable = null, string overrideId = null)
+    public ChunkGenerator GetNextRoom(ChunkGenerator previousRoom)
     {
-        string id = overrideId ?? roomData[x, y].Id;
-        string newId = id + "_";
-        if ((x, y) is (ChunkSize - 1, ChunkSize - 1)) return DataHolder.TileTypes[newId + "nesw"];
-        StringBuilder wallKey = new StringBuilder(newId);
-        if (northernTileConnectable ?? CheckForConnectionInTile(roomData, x, y + 1)) wallKey.Append('n');
-        if (easternTileConnectable ?? CheckForConnectionInTile(roomData, x + 1, y)) wallKey.Append('e');
-        if (CheckForConnectionInTile(roomData, x, y - 1)) wallKey.Append('s');
-        if (CheckForConnectionInTile(roomData, x - 1, y)) wallKey.Append('w');
-        string str = wallKey.ToString();
-        if (str[^1] == '_') str = id;
-        return DataHolder.TileTypes[str];
-    }
-
-    private static bool CheckForConnectionInTile(TileTypeData[,] roomData, int x, int y)
-    {
-        if (!(x is >= -1 and < ChunkSize && y is >= -1 and < ChunkSize)) return false;
-        return x is -1 or ChunkSize - 1 || y is -1 or ChunkSize - 1 || roomData[x, y].IsConnection;
-    }
-
-    private void ResolveRoomConnectionDependency(TileTypeData[,] roomData, int x, int y)
-    {
-        string[] parsedId = roomData[x, y].Id.Split('_');
-        bool switchId = parsedId[2] switch
-        {
-            "north" => NorthConnection is null,
-            "east" => EastConnection is null,
-            "south" => SouthConnection is null,
-            "west" => WestConnection is null,
-            _ => false
-        };
-
-        string newId = !switchId ? parsedId[0] : parsedId[1];
-        roomData[x, y] = DataHolder.TileTypes[newId];
+        if (NorthConnection is not null && NorthConnection != previousRoom) return NorthConnection;
+        if (EastConnection is not null && EastConnection != previousRoom) return EastConnection;
+        if (SouthConnection is not null && SouthConnection != previousRoom) return SouthConnection;
+        return WestConnection!;
     }
 
     public char CalculateSymbol()
@@ -214,5 +156,64 @@ public class ChunkGenerator
         }
 
         return symbols[0];
+    }
+
+    private void FillBorderWalls(TileTypeData[,] roomData, Tile[,] northernTileMap, Tile[,] easternTileMap)
+    {
+        for (int x = 0; x < ChunkSize; x++)
+        {
+            TileTypeData terrainObject = NorthConnection is null || x is not (ChunkSize / 2 - 1 or ChunkSize / 2)
+                ? GetConnectableData(roomData, x, ChunkSize - 1, northernTileMap?[x, 0].ConnectToWalls,
+                    overrideId: "wall")
+                : DataHolder.TileTypes["floor"]; //will be changed to "door" later
+            Tiles[x, ChunkSize - 1] = new Tile(terrainObject);
+        }
+
+        for (int y = 0; y < ChunkSize - 1; y++)
+        {
+            TileTypeData terrainObject = EastConnection is null || y is not (ChunkSize / 2 - 1 or ChunkSize / 2)
+                ? GetConnectableData(roomData, ChunkSize - 1, y,
+                    easternTileConnectable: easternTileMap?[0, y].ConnectToWalls, overrideId: "wall")
+                : DataHolder.TileTypes["floor"]; //will be changed to "door" later
+            Tiles[ChunkSize - 1, y] = new Tile(terrainObject);
+        }
+    }
+
+    private TileTypeData GetConnectableData(TileTypeData[,] roomData, int x, int y,
+        bool? northernTileConnectable = null, bool? easternTileConnectable = null, string overrideId = null)
+    {
+        string id = overrideId ?? roomData[x, y].Id;
+        string newId = id + "_";
+        if ((x, y) is (ChunkSize - 1, ChunkSize - 1)) return DataHolder.TileTypes[newId + "nesw"];
+        StringBuilder wallKey = new StringBuilder(newId);
+        if (northernTileConnectable ?? CheckForConnectionInTile(roomData, x, y + 1)) wallKey.Append('n');
+        if (easternTileConnectable ?? CheckForConnectionInTile(roomData, x + 1, y)) wallKey.Append('e');
+        if (CheckForConnectionInTile(roomData, x, y - 1)) wallKey.Append('s');
+        if (CheckForConnectionInTile(roomData, x - 1, y)) wallKey.Append('w');
+        string str = wallKey.ToString();
+        if (str[^1] == '_') str = id;
+        return DataHolder.TileTypes[str];
+    }
+
+    private void ResolveRoomConnectionDependency(TileTypeData[,] roomData, int x, int y)
+    {
+        string[] parsedId = roomData[x, y].Id.Split('_');
+        bool switchId = parsedId[2] switch
+        {
+            "north" => NorthConnection is null,
+            "east" => EastConnection is null,
+            "south" => SouthConnection is null,
+            "west" => WestConnection is null,
+            _ => false
+        };
+
+        string newId = !switchId ? parsedId[0] : parsedId[1];
+        roomData[x, y] = DataHolder.TileTypes[newId];
+    }
+
+    private static bool CheckForConnectionInTile(TileTypeData[,] roomData, int x, int y)
+    {
+        if (!(x is >= -1 and < ChunkSize && y is >= -1 and < ChunkSize)) return false;
+        return x is -1 or ChunkSize - 1 || y is -1 or ChunkSize - 1 || roomData[x, y].IsConnection;
     }
 }
