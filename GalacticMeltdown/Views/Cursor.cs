8dde2eca48a8609c12303de1cef67b97a6adf77c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-11 21:18:51 +0300

    Add cursor methods and properties

--- GalacticMeltdown/Views/Cursor.cs
+++ GalacticMeltdown/Views/Cursor.cs
@@ -1,6 +1,23 @@
+using System;
+using GalacticMeltdown.Events;
+using GalacticMeltdown.LevelRelated;
+
 namespace GalacticMeltdown.Views;
 
-public class Cursor
+public class Cursor : IControllable
 {
+    public (char symbol, ConsoleColor color) SymbolData { get; }
+    public ConsoleColor? BgColor { get; }
     
+    public int X { get; }
+    public int Y { get; }
+    
+    public event EventHandler<MoveEventArgs> Moved;
+    
+    public bool InFocus { get; set; }
+    
+    public bool TryMove(int deltaX, int deltaY)
+    {
+        return true;
+    }
 }
