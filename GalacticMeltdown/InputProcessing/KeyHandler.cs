630d276d2e475d91b737a2052d06ca9c2c416c3f
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-15 17:24:00 +0300

    Add KeyHandler

---
+++ GalacticMeltdown/InputProcessing/KeyHandler.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace GalacticMeltdown.InputProcessing;
+
+public abstract class KeyHandler
+{
+    private bool _flushBuffer;
+    
+    protected KeyHandler(bool flushBuffer = false)
+    {
+        _flushBuffer = flushBuffer;
+    }
+
+    public virtual void HandleKey(ConsoleKey key)
+    {
+        if (_flushBuffer) FlushBuffer();
+    }
+
+    private void FlushBuffer()
+    {
+        while (Console.KeyAvailable) Console.ReadKey(true);
+    }
+}
