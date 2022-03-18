eaf91ac4fa9a0a1409308a0c6221396eb034199f
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-19 02:23:46 +0300

    Add TextLine

---
+++ GalacticMeltdown/UserInterfaceRelated/Rendering/TextLine.cs
@@ -0,0 +1,31 @@
+using System;
+using GalacticMeltdown.Data;
+using GalacticMeltdown.Views;
+
+namespace GalacticMeltdown.UserInterfaceRelated.Rendering;
+
+public class TextLine : ListLine
+{
+    private const ConsoleColor BgColor = DataHolder.Colors.BackgroundColorUnselected;
+    private const ConsoleColor TextColor = DataHolder.Colors.TextColor;
+
+    private readonly string _text;
+    private string _renderedText;
+
+    public TextLine(string text)
+    {
+        _text = text;
+    }
+
+    public override void SetWidth(int width)
+    {
+        const string ellipsis = "...";
+
+        _renderedText = _text.Length > width
+            ? _text.Substring(0, width - ellipsis.Length) + ellipsis
+            : _text.PadRight(width);
+        base.SetWidth(width);
+    }
+
+    public override ViewCellData this[int x] => new((_renderedText[x], TextColor), BgColor);
+}
