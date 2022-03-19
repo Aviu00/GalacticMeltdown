ec998315d777a72156119a427a64af5bea7c2795
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-19 16:34:05 +0300

    Add StatInfo

--- GalacticMeltdown/Views/OverlayView.cs
+++ GalacticMeltdown/Views/OverlayView.cs
@@ -5,11 +5,31 @@ using GalacticMeltdown.LevelRelated;
 
 namespace GalacticMeltdown.Views;
 
+internal struct StatInfo
+{
+    public string Text;
+    public int Val;
+    public int? MaxVal;
+
+    public StatInfo(string text, int val, int? maxVal = null)
+    {
+        Text = text;
+        Val = val;
+        MaxVal = maxVal;
+    }
+}
+
 public class OverlayView : View
 {
     private Level _level; // Minimap, Enemy-in-sight indicator, (?) Event log
     private Player _player; // State, Effects, (?) Event log, Enemy-in-sight indicator
     // Performance monitor? Coordinates of controlled object, player?
+
+    private StatInfo _hpInfo;
+    private StatInfo _energyInfo;
+    private StatInfo _dexInfo;
+    private StatInfo _defInfo;
+    private StatInfo _strInfo;
     
     public override event EventHandler NeedRedraw;
     public override event EventHandler<CellChangeEventArgs> CellsChanged;
@@ -25,6 +45,10 @@ public class OverlayView : View
 
     public override ViewCellData GetSymbol(int x, int y)
     {
+        if (y == Width - 1)
+        {
+            
+        }
         return new ViewCellData(null, null);
     }
 }
