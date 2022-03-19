cd6672d40eec8ea80b530340524e977b6f106184
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-19 15:46:40 +0300

    Remove unneeded fields from OverlayView

--- GalacticMeltdown/Views/OverlayView.cs
+++ GalacticMeltdown/Views/OverlayView.cs
@@ -10,8 +10,6 @@ public class OverlayView : View
     private Level _level; // Minimap, Enemy-in-sight indicator, (?) Event log
     private Player _player; // State, Effects, (?) Event log, Enemy-in-sight indicator
     // Performance monitor? Coordinates of controlled object, player?
-
-    private ViewCellData[,] _symbols;
     
     public override event EventHandler NeedRedraw;
     public override event EventHandler<CellChangeEventArgs> CellsChanged;
@@ -27,15 +25,6 @@ public class OverlayView : View
 
     public override ViewCellData GetSymbol(int x, int y)
     {
-        if (!(0 <= x && x < Width && 0 <= y && y < Height)) return new ViewCellData(null, null);
-        return _symbols[x, y];
-    }
-
-    public override void Resize(int width, int height)
-    {
-        int oldWidth = Width, oldHeight = Height;
-        base.Resize(width, height);
-        if (oldWidth == Width && oldHeight == Height) return;
-        _symbols = new ViewCellData[Width, Height];
+        return new ViewCellData(null, null);
     }
 }
