c7b05ca6da2b00425cf7c38b6b95cf2f8b7e034d
Ruslan Abliazov <r.abliazov@gmail.com>
2022-03-20 22:30:03 +0300

    add MeleeAttackStrategy file

--- GalacticMeltdown/Behaviors/MeleeAttackStrategy.cs
+++ GalacticMeltdown/Behaviors/MeleeAttackStrategy.cs
@@ -1,5 +1,6 @@
-using System;
 using System.Linq;
+using GalacticMeltdown.Behaviors;
+using System;
 using GalacticMeltdown.Actors;
 using GalacticMeltdown.Data;
 using GalacticMeltdown.Utility;
@@ -16,7 +17,7 @@ public class MeleeAttackStrategy : Behavior
     [JsonProperty] private readonly int _cooldown;
     [JsonProperty] private readonly int _meleeAttackCost;
     [JsonIgnore] private Counter meleeAtackCounter;
-    
+
     public MeleeAttackStrategy(MeleeAttackStrategyData data, Npc controlledNpc) : base(data.Priority ?? DefaultPriority)
     {
         _minDamage = data.MinDamage;
@@ -26,10 +27,10 @@ public class MeleeAttackStrategy : Behavior
         ControlledNpc = controlledNpc;
         if (_cooldown > 0)
         {
-            meleeAtackCounter = new Counter(ControlledNpc.Level, _cooldown, 0);   
+            meleeAtackCounter = new Counter(ControlledNpc.Level, _cooldown, 0);
         }
     }
-    
+
     public override bool TryAct()
     {
         if (ControlledNpc.CurrentTarget is null)
@@ -42,12 +43,10 @@ public class MeleeAttackStrategy : Behavior
             ControlledNpc.Energy -= _meleeAttackCost;
             if (meleeAtackCounter is not null)
                 meleeAtackCounter.ResetTimer();
-            return true;
         }
 
         return false;
     }
-
     // TODO: make advanced random damage 
     private int RandomDamage(int minDamage, int maxDamage)
     {
