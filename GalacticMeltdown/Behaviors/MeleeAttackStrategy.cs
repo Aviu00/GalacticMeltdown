c808540fae3f4438c0acb0fe53da055283ddbdda
Ruslan Abliazov <r.abliazov@gmail.com>
2022-03-21 16:26:21 +0300

    add counters to event

--- GalacticMeltdown/Behaviors/MeleeAttackStrategy.cs
+++ GalacticMeltdown/Behaviors/MeleeAttackStrategy.cs
@@ -30,6 +30,7 @@ public class MeleeAttackStrategy : Behavior
         if (data.Cooldown > 0)
         {
             _meleeAttackCounter = new Counter(ControlledNpc.Level, data.Cooldown, 0);
+            ControlledNpc.Died += _meleeAttackCounter.RemoveCounter;
         }
     }
     
