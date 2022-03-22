fc62d26c0331f052a0d1b8d3c4349530adce38de
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-22 20:34:01 +0300

    Add ActorActionEventArgs

---
+++ GalacticMeltdown/Events/ActorActionEventArgs.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using GalacticMeltdown.ActorActions;
+
+namespace GalacticMeltdown.Events;
+
+public class ActorActionEventArgs : EventArgs
+{
+    public ActorAction Action { get; }
+    public List<(int, int)> AffectedCells { get; }
+
+    public ActorActionEventArgs(ActorAction action, List<(int, int)> affectedCells)
+    {
+        Action = action;
+        AffectedCells = affectedCells;
+    }
+}
