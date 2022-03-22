0bd3ff9266e7efea90e32191aebedf4631cbe851
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-22 12:18:05 +0300

    Add EndGameMessage

---
+++ GalacticMeltdown/UserInterfaceRelated/TextWindows/EndGameMessage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using GalacticMeltdown.Data;
+using GalacticMeltdown.UserInterfaceRelated.InputProcessing;
+using GalacticMeltdown.UserInterfaceRelated.InputProcessing.ControlTypes;
+using GalacticMeltdown.UserInterfaceRelated.Rendering;
+using GalacticMeltdown.Utility;
+
+namespace GalacticMeltdown.UserInterfaceRelated.TextWindows;
+
+public class EndGameMessage : TextWindow
+{
+    public EndGameMessage(string message)
+    {
+        LineView.SetLines(new List<ListLine>
+        {
+            new TextLine(message),
+            new Button("OK", "", Close)
+        });
+        Controller = new ActionHandler(UtilityFunctions.JoinDictionaries(DataHolder.CurrentBindings.Selection,
+            new Dictionary<SelectionControl, Action>
+            {
+                {SelectionControl.Select, LineView.PressCurrent}
+            }));
+    }
+}
