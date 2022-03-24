78950a113ab3cda07b66f24ff47bf1a5f6c3e926
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-24 20:45:23 +0300

    Reimplement View positioners

--- GalacticMeltdown/UserInterfaceRelated/Rendering/BasicViewPositioner.cs
+++ GalacticMeltdown/UserInterfaceRelated/Rendering/BasicViewPositioner.cs
@@ -1,13 +1,21 @@
+using System.Collections.Generic;
+using GalacticMeltdown.Views;
+
 namespace GalacticMeltdown.UserInterfaceRelated.Rendering;
 
 public class BasicViewPositioner : ViewPositioner
 {
+    private View _view;
+    
+    public BasicViewPositioner(View view)
+    {
+        _view = view;
+    }
+
     public override void SetScreenSize(int width, int height)
     {
         base.SetScreenSize(width, height);
-        foreach (var view in ViewPositions.Keys)
-        {
-            ViewPositions[view] = (0, 0, Width, Height);
-        }
+        _view.Resize(Width, Height);
+        ViewPositions = new List<(View, int minX, int minY, int maxX, int maxY)> {(_view, 0, 0, Width, Height)};
     }
 }
--- GalacticMeltdown/UserInterfaceRelated/Rendering/ViewPositioner.cs
+++ GalacticMeltdown/UserInterfaceRelated/Rendering/ViewPositioner.cs
@@ -7,17 +7,11 @@ public abstract class ViewPositioner
 {
     protected int Width;
     protected int Height;
-    protected List<View> _views;
-    protected Dictionary<View, (int minX, int minY, int maxX, int maxY)> ViewPositions;
+    protected List<(View, int minX, int minY, int maxX, int maxY)> ViewPositions;
 
     public virtual void SetScreenSize(int width, int height)
     {
         Width = width;
         Height = height;
-    }
-
-    public (int minX, int minY, int maxX, int maxY) GetPos(View view)
-    {
-        return ViewPositions[view];
     }
 }
