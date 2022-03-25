15d9cff4aa5149ed8360a323159ab67b4660f09c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-25 19:33:48 +0300

    Add AmmoDialog

---
+++ GalacticMeltdown/UserInterfaceRelated/TextWindows/AmmoDialog.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GalacticMeltdown.Actors;
+using GalacticMeltdown.Items;
+
+namespace GalacticMeltdown.UserInterfaceRelated.TextWindows;
+
+public class AmmoDialog : ChoiceDialog<string>
+{
+    public AmmoDialog(Player player, Action<string> send) : base(GetChoices(player), send, "Choose ammo type")
+    {
+    }
+
+    private static List<(string text, string choice)> GetChoices(Player player)
+    {
+        var availableAmmoTypes = player.Inventory[ItemCategory.Item]
+            .DistinctBy(item => item.Id)
+            .Where(item => ((WeaponItem) player.Equipment[BodyPart.Hands]).AmmoTypes.ContainsKey(item.Id));
+        var choices = new List<(string text, string choice)> {("Unset ammo", null)};
+        choices.AddRange(availableAmmoTypes.Select(item => (item.Name, item.Id)));
+        return choices;
+    }
+}
