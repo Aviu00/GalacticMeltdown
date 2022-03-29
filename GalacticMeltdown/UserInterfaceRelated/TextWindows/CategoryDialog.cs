8b55c01419ae6b5e375c49e34c29d19a9da03d7d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-03-29 14:11:45 +0300

    Add CategoryDialog

---
+++ GalacticMeltdown/UserInterfaceRelated/TextWindows/CategoryDialog.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GalacticMeltdown.Data;
+using GalacticMeltdown.Items;
+
+namespace GalacticMeltdown.UserInterfaceRelated.TextWindows;
+
+public class CategoryDialog : ChoiceDialog<ItemCategory?>
+{
+    public CategoryDialog(Action<ItemCategory?> send) : base(GetChoices(), send)
+    {
+    }
+
+    private static List<(string text, ItemCategory? choice)> GetChoices()
+    {
+        var choices = new List<(string text, ItemCategory? choice)> { ("All", null) };
+        choices.AddRange(Enum.GetValues<ItemCategory>()
+            .Select(category => (DataHolder.CategoryName[category], (ItemCategory?) category)));
+        return choices;
+    }
+}
