ffe1b145ea88bad0c6a6030fe99a54a6c172a565
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-04-01 21:10:25 +0300

    Rewrite InputProcessor

--- GalacticMeltdown/UserInterfaceRelated/InputProcessing/InputProcessor.cs
+++ GalacticMeltdown/UserInterfaceRelated/InputProcessing/InputProcessor.cs
@@ -8,105 +8,60 @@ namespace GalacticMeltdown.UserInterfaceRelated.InputProcessing;
 public class InputProcessor
 {
     private Dictionary<object, Controller> _objectControllers;
-    private HashSet<Controller> _dormantControllers;
     private OrderedSet<Controller> _activeControllers;
     
     private bool _inLoop;
-    private Controller _currentController;
 
     public InputProcessor()
     {
         _objectControllers = new Dictionary<object, Controller>();
-        _dormantControllers = new HashSet<Controller>();
         _activeControllers = new OrderedSet<Controller>();
     }
 
     public void YieldControl(object sender)
     {
-        if (!_objectControllers.ContainsKey(sender)) return;
-
-        Controller controller = _objectControllers[sender];
-        if (controller != _currentController) return;
-        _dormantControllers.Add(controller);
-        RemoveCurrentController();
+        if (!_objectControllers.TryGetValue(sender, out Controller controller)) return;
+        _activeControllers.Remove(controller);
     }
 
     public void TakeControl(object sender)
     {
-        if (!_objectControllers.ContainsKey(sender)) return;
-        
-        Controller controller = _objectControllers[sender];
-        if (controller == _currentController) return;
-        
-        if (_dormantControllers.Contains(controller))
-        {
-            _dormantControllers.Remove(controller);
-            SetCurrentController(controller);
-        }
-        else if (_activeControllers.Contains(controller))
-        {
-            _activeControllers.Remove(controller);
-            SetCurrentController(controller);
-        }
-    }
-
-    private void SetCurrentController(Controller controller)
-    {
-        if (_currentController is not null)
-        {
-            _activeControllers.Add(_currentController);
-        }
-
-        _currentController = controller;
+        if (!_objectControllers.TryGetValue(sender, out Controller controller)) return;
+        _activeControllers.Remove(controller);
+        _activeControllers.Add(controller);
         if (!_inLoop) Loop();
     }
 
     public void SetController(object sender, Controller controller)
     {
-        if (_objectControllers.ContainsKey(sender))
+        if (_objectControllers.TryGetValue(sender, out Controller oldController))
         {
-            Controller oldHandler = _objectControllers[sender];
-            _dormantControllers.Remove(oldHandler);
-            _activeControllers.Remove(oldHandler);
-            if (_currentController == oldHandler)
-            {
-                _currentController = controller;
-            }
+            _activeControllers.Remove(oldController);
         }
-        else
+
+        if (controller is null)
         {
-            _dormantControllers.Add(controller);
+            _objectControllers.Remove(sender);
+            return;
         }
-        
+
         _objectControllers[sender] = controller;
     }
 
     public void RemoveController(object obj)
     {
-        if (!_objectControllers.ContainsKey(obj)) return;
-        Controller controller = _objectControllers[obj];
-        _dormantControllers.Remove(controller);
+        if (!_objectControllers.TryGetValue(obj, out Controller controller)) return;
         _activeControllers.Remove(controller);
-        if (_currentController == controller)
-        {
-            RemoveCurrentController();
-        }
         _objectControllers.Remove(obj);
-    }
-
-    private void RemoveCurrentController()
-    {
-        _currentController = null;
-        if (!_activeControllers.Any()) return;
-        _currentController = _activeControllers.Pop();
     }
 
     private void Loop()
     {
         _inLoop = true;
-        while (_currentController is not null)
+        Controller active;
+        while ((active = _activeControllers.Last) is not null)
         {
-            _currentController.HandleKey(Console.ReadKey(true));
+            active.HandleKey(Console.ReadKey(true));
         }
 
         _inLoop = false;
